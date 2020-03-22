23fbab756308049bc1b80ca6beb109a5be4f232d
Bhupen Chauhan <bhupen.chauhan@reverieinc.com>
2020-03-22 14:47:49 +0530

    trial: remove unwanted domains

---
+++ erpnext/domains/generic.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+data = {
+	'desktop_icons': [
+		'Project',
+		'Timesheet',
+		'Task',
+		'Expense Claim',
+		'Employee',
+		'HR',
+		'ToDo'
+	],
+	'set_value': [
+		['Stock Settings', None, 'show_barcode_field', 0]
+	],
+	'default_portal_role': 'Customer'
+}
